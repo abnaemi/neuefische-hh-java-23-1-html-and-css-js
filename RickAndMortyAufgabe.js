bf4e3870ca7655fe3e97b85a6b783b5c4d8b66f8
abnae <ab.naemi@gmail.com>
2023-03-21 18:29:40 +0100

    Added RM API Methods

--- RickAndMortyAufgabe.js
+++ RickAndMortyAufgabe.js
@@ -262,3 +262,24 @@ var response = {
             "created": "2017-11-04T22:34:53.659Z"
         }]
 };
+function getAllHumansAlive(characters) {
+    return characters.filter(function (character) {
+        return character.status === "Alive" && character.species === "Human";
+    });
+}
+var charactersAlive = getAllHumansAlive(response.results);
+console.log(charactersAlive);
+function getAllCharactersNames(characters) {
+    return characters.map(function (character) {
+        return character.name;
+    });
+}
+var allCharacters = getAllCharactersNames(response.results);
+console.log(allCharacters);
+function getAllCharactersAsObject(characters) {
+    return characters.map(function (character) {
+        return { name: character.name, origin: character.origin.name };
+    });
+}
+var characters = getAllCharactersAsObject(response.results);
+console.log(characters);
